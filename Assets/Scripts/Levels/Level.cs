125cf7cb29830d47b6af634a8478496cdb6aebab
Kadochnikov <mkad@ceramic3d.ru>
2021-02-08 11:41:26 +0500

    +BaseLevel

---
+++ Assets/Scripts/Levels/Level.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Levels
+{
+    public class Level : MonoBehaviour
+    {
+
+        public event Action OnSequanceEnded;
+        public Thesis Current => this._theses?[this._index];
+        public void MoveNext()
+        {
+            this._index++;
+            if(this._index > this._theses.Length)
+            {
+                this.OnSequanceEnded?.Invoke();
+            }
+        }
+        private void Awake()
+        {
+            this._index = 0;
+        }
+
+        public static Level Create(params Thesis[] theses)
+        {
+            GameObject go = new GameObject("Level");
+            Level level = go.AddComponent<Level>();
+            level._theses = theses;
+            return level;
+        }
+
+
+
+        private int _index;
+        private Thesis[] _theses;
+    }
+}
---
+++ Assets/Scripts/Levels/Thesis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messaging;
+
+namespace Levels
+{
+    public class Thesis
+    {
+        public string Text { get; }
+        public MessageType Type { get; }
+        public float Pause { get; }
+        public Thesis(string text, MessageType type, float pause = 0f)
+        {
+            this.Text = text;
+            this.Type = type;
+            this.Pause = pause;
+        }
+    }
+}
